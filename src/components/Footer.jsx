6a65a8c038a3b5b6fd91cb8e16c94c844dc64339
Manuelito18 <jomafersan14@gmail.com>
2025-07-04 16:08:10 -0500

    Actualizando el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,69 @@
 // src/components/Footer.jsx
+import { FaGithub, FaEnvelope, FaYoutube, FaBook, FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 import styles from "./styles/Footer.module.css";
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>© 2025 Colmenares – Música ficticia, código real 🎧</p>
+      <div className={styles.content}>
+        <p>© 2025 Colmenares – Música ficticia, código real 🎧</p>
+        <nav className={styles.social}>
+          <a
+            href="https://github.com/Manuelito18/colmenares-react"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+          >
+            <FaGithub />
+          </a>
+          <a
+            href="https://www.facebook.com/colmemusic/?locale=es_LA"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Facebook"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            href="https://www.instagram.com/reel/DKwa0D4gFPO/?utm_source=ig_web_copy_link"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="https://wa.me/+51979224000"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="WhatsApp"
+          >
+            <FaWhatsapp />
+          </a>
+          <a
+            href="mailto:mancol@hotmail.com"
+            aria-label="Email"
+          >
+            <FaEnvelope />
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCBYloQzyO20TZfttyNlC_Lg"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="YouTube"
+          >
+            <FaYoutube />
+          </a>
+          <a
+            href="https://colmenares.com/libro-de-reclamaciones"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Libro de Reclamaciones"
+          >
+            <FaBook />
+          </a>
+        </nav>
+      </div>
     </footer>
   );
 }
