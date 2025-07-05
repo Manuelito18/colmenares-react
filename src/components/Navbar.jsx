5f077622407060ae87d7cdab30592cd57a6657cf
Manuelito18 <jomafersan14@gmail.com>
2025-07-05 14:08:27 -0500

    Transparencia y movimiento en el nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,30 @@
 // src/components/Navbar.jsx
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import styles from "./styles/Navbar.module.css";
 
 export default function Navbar() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const manejarScroll = () => {
+      setScrolled(window.scrollY > 10);
+    };
+    window.addEventListener("scroll", manejarScroll);
+    return () => window.removeEventListener("scroll", manejarScroll);
+  }, []);
+
   return (
-    <nav className={styles.navbar}>
+    <nav className={`${styles.navbar} ${scrolled ? styles.scrolled : ""}`}>
+      <div className={styles.navInner}>
         <img className={styles.logo} src="/imgs/logonar.png" alt="Logo colmenares" />
-      <ul className={styles.navLinks}>
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/nosotros">Nosotros</Link></li>
-        <li><Link to="/productos">Productos</Link></li>
-        <li><Link to="/contacto">Contacto</Link></li>
-      </ul>
+        <ul className={styles.navLinks}>
+          <li><Link to="/">Inicio</Link></li>
+          <li><Link to="/nosotros">Nosotros</Link></li>
+          <li><Link to="/productos">Productos</Link></li>
+          <li><Link to="/contacto">Contacto</Link></li>
+        </ul>
+      </div>
     </nav>
   );
 }
