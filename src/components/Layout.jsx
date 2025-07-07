e6c6aef0bde0054fd6f88608aaa2bb8112f0a6db
Manuelito18 <jomafersan14@gmail.com>
2025-07-07 17:59:34 -0500

    style(footer): Normalizado

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,6 @@ import Header from "./Header";
 import Footer from "./Footer";
 import styles from "./styles/Layout.module.css";
 import { Outlet } from "react-router-dom";
-
 import ScrollToTop from "./specials/ScrollToTop";
 
 export default function Layout() {
