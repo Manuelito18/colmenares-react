393ce28252ca45e6bd7d66a12e3c124ec23a9aaa
Manuelito18 <jomafersan14@gmail.com>
2025-07-07 20:08:51 -0500

    feat(heroSlider): nuevo carruse mejorado, import imgs, new data js

---
+++ src/data/promos.js
@@ -0,0 +1,14 @@
+export const promos = [
+  {
+    image: "/imgs/guitarra.png", // Debe estar en /public/images/slider1.jpg
+    title: "Guitarras Eléctricas",
+    subtitle: "Aprovecha el 20% de descuento",
+    description: "La musica comienza aquí. Explora nuestra colección de guitarras eléctricas.",
+  },
+  {
+    image: "/imgs/piano.jpg",
+    title: "Pianos Digitales",
+    subtitle: "Sonidos que inspiran",
+    description: "Descubre la elegancia y versatilidad de nuestros pianos digitales.",
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,22 @@
 import styles from "./styles/Home.module.css";
-import PromoCarousel from "../components/PromoCarousel";
-import { imagenesPromo } from "../data/img-carrusel";
+import HeroSlider from "../components/HeroSlider";
+import { promos } from "../data/promos";
 
 export default function Home() {
   return (
     <>
-    <section className={styles.home}>
-      <h1 className={styles.title}>Bienvenido a Colmenares 🎶</h1>
-      <p className={styles.subtitle}>
-        Encuentra guitarras, pianos, sintetizadores y más con un estilo que resuena.
-      </p>
-    </section>
-    <section>
-      <PromoCarousel imagenes={imagenesPromo}/>
-    </section>
+      <section>
+        {/* <PromoCarousel imagenes={imagenesPromo}/> */}
+        <HeroSlider slides={promos} />
+      </section>
+      <section className={styles.home}>
+        <h1 className={styles.title}>Bienvenido a Colmenares 🎶</h1>
+        <p className={styles.subtitle}>
+          Encuentra guitarras, pianos, sintetizadores y más con un estilo que
+          resuena.
+        </p>
+      </section>
     </>
   );
 }
+1;
