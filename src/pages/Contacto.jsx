d4e0196c7c66973ce1ee4427eb03fa47edb92ac7
Manuelito18 <jomafersan14@gmail.com>
2025-07-10 16:40:53 -0500

    fix(Contacto): style fix auto responsive

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -9,6 +9,19 @@ export default function Contacto() {
         text="¿Tienes dudas, sugerencias o simplemente quieres hablar de guitarras? ¡Escríbenos!"
         background="linear-gradient(135deg, #591C86, #821844)"
       />
+      <div className={styles.container}>
+        <div className={styles.contact}>
+          {/* 4 bloques con informacion de contacto */}
+        </div>
+        <form className={styles.form}>{/* haz el formulario */}</form>
+      </div>
+      <div className={styles.ubication}>
+        <p>Nuestra Ubicación</p>
+        {/* Mapa de google maps */}
+      </div>
+      <div className={styles.questions}>
+        {/* 4 bloques con preguntas frecuentes */}
+      </div>
     </section>
   );
 }
