f64e526b5aad568cf482092b9178268b493ee275
Manuelito18 <jomafersan14@gmail.com>
2025-07-11 00:42:45 -0500

    chore(card personas): update card persons

--- src/components/CardPer.jsx
+++ src/components/CardPer.jsx
@@ -1,12 +1,31 @@
 import styles from "./styles/CardPer.module.css";
+import { FaGithub } from "react-icons/fa"; // Asegúrate de tener react-icons instalado
 
-export default function CardPer({ img = null, name, role, des }) {
+export default function CardPer({
+  img = null,
+  name,
+  role,
+  des,
+  github = null,
+}) {
   return (
     <div className={styles.card}>
       <img className={styles.photo} src={img} alt="image" />
       <h3 className={styles.name}>{name}</h3>
       <h4 className={styles.role}>{role}</h4>
       <p className={styles.description}>{des}</p>
+
+      {github && (
+        <a
+          href={github}
+          className={styles.githubIcon}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={`${name}'s GitHub`}
+        >
+          <FaGithub />
+        </a>
+      )}
     </div>
   );
 }
