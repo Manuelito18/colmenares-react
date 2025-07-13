40e4f0e8838d0beccc552f330cc18df915b301b7
Manuelito18 <jomafersan14@gmail.com>
2025-07-12 23:52:16 -0500

    chore(users.js): update users

---
+++ src/data/users.js
@@ -0,0 +1,39 @@
+export const users = [
+  {
+    img: "https://unavatar.io/github/manuelito18",
+    name: "José Manuel Fernández Sandoval",
+    role: "Programador",
+    des: "modo xd",
+    github: "https://github.com/manuelito18",
+  },
+  {
+    img: "https://i.pinimg.com/736x/47/04/4a/47044a1c77d6fedb822e4511dbc4a945.jpg",
+    name: "Juan Carlos Córdova Barrantes",
+    role: "Desarrollador Web - Full Stack",
+    des: "Me especializo en crear soluciones eficientes, escalables y centradas en el usuario. Trabajo bien en equipo, me adapto rápido y siempre busco aprender nuevas herramientas para mejorar mis proyectos",
+    github: null,
+  }
+]
+/*
+<div className={styles.teamUser}>
+  <CardPer
+    img="https://unavatar.io/github/furry"
+    name="Pietro"
+    role="Furry"
+    des="La vida solo tiene sentido si hay furrys en ella."
+  />
+  <CardPer
+    github="https://github.com/manuelito18/"
+    img="https://unavatar.io/github/manuelito18"
+    name="Manuel"
+    role="Otaku"
+    des="Solo se que otaku fui y otaku seré. Kurumi mi Diosa"
+  />
+  <CardPer
+    img="https://unavatar.io/x/lolicon"
+    name="Josue"
+    role="Lolicon"
+    des="La edad es solo un número, lo que realmente importa es que sean lolis"
+  />
+</div>
+*/
